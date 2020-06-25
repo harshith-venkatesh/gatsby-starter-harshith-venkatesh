63b2e5e56d704a10893c1b78efc6a88ecb3343a0
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-06-26 05:07:04 +0530

    update

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
   manifestName: 'Resume',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
+  manifestBackgroundColor: '#F8AD0B',
+  manifestThemeColor: '#F8AD0B',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   //pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
